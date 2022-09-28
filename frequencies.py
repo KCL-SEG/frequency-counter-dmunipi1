818d7dbe33751e59beb5cedfe6d2ff71cb0ddaba
Dion Munipi <dion.munipi@kcl.ac.uk>
2022-09-28 13:26:40 +0100

    Create frequency function

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,14 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if not isinstance(item, str):
+            item = str(item)
+        if item in frequencies:
+            count = frequencies[item]
+            count += 1
+            frequencies.pop(item)
+            frequencies[item] = count
+        else:
+            frequencies[item] = 1
     return frequencies
